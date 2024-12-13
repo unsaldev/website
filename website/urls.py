1c892883b4e8635672a50814c39d0714994720cb
unsaldev <unsal_dev@aureonsoft.com.tr>
2024-12-14 01:24:51 +0300

    updated

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView  # TemplateView'ı import ediyoruz
 from catalog.views import ReactAppView  # ReactAppView'ı import ediyoruz
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("cms/admin/", admin.site.urls),
    # path('', TemplateView.as_view(template_name='index.html'), name='home'),  # index.html'yi burada çağırıyoruz
     path('', ReactAppView.as_view(), name='home')
    # path("api/", include('catalog.urls')),  # catalog/urls.py'yi dahil ettik
